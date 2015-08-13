974f88c0fc501d99dd18b943dcf2c4ba945d0d27
LukeHaines <lukehaines2@gmail.com>
2015-08-13 19:41:33 +0100

    basics coming in

--- advanced_calc/js/main.js
+++ advanced_calc/js/main.js
@@ -1,1 +1,20 @@
-console.log("hi");
+
+1. write an event listener on the basic-calc button
+2. inside the function on that event listener collect the values from the user and store them in variables
+3. do the relevant calculations on those variables
+4. show the answer div
+5. update the text of the answer div accordingly
+
+
+
+var num1 = parseFloat(document.getElementById("basic-num-1").value);
+console.log(num1)
+var num2 = parseFloat(document.getElementById("basic-num-2").value);
+console.log(num2)
+var op = document.getElementById("basic-operation").value);
+console.log(op)
+
+button1.addEventListener("click", function(){
+  console.log("click button event triggered");
+});
+
