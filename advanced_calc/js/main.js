b1bf029046cff9e9caa8c0a4cedc6df399d1e0bd
LukeHaines <lukehaines2@gmail.com>
2015-08-13 21:54:18 +0100

    got BMI working

--- advanced_calc/js/main.js
+++ advanced_calc/js/main.js
@@ -45,28 +45,37 @@ button1.addEventListener("click", function(){
 
 // BMI CALC
 var button3 = document.getElementById("bmi-calc");
-button1.addEventListener("click", function(){
+
+button3.addEventListener("click", function() {
   console.log("click button event triggered");
 
   var mass = parseFloat(document.getElementById("bmi-mass").value);
   console.log(mass)
-  var height = parseFloat(document.getElementById("bmi-units").value);
+  var height = parseFloat(document.getElementById("bmi-height").value);
   console.log(height)
   var units = document.getElementById("bmi-units").value;
   console.log(units)
+  if (units === "metric") {
+      var bmi = mass / Math.pow(height, 2);
+    } else {
+      var bmi = (mass * 703) / Math.pow(height, 2);
+    };
+       console.log(mass);
 
-      var answer;
+      var bmi; 
       // Calculate the answer based on the chosen basic operator
       
-      bmi = mass / Math.pow(height, 2);
+      
       bmi = Math.round(bmi * 100) / 100;
 
-      if units = KG then if {units * 708}
       // finally, display the result
   var bmiAnswer = document.getElementById("bmi-answer");
 
+  // answer = bmi
+
   bmiAnswer.className = "show";
 
-  document.getElementById("bmi-answer").innerHTML = "Your BMI is : " + bmi;
+  document.getElementById("bmi-answer-alert").innerHTML = " Your BMI is : " + bmi;
 
 });
+
