d01a3765037a64406daffbfb686fd70bb0c9a897
LukeHaines <lukehaines2@gmail.com>
2015-08-13 20:46:58 +0100

    tackling BMI

--- advanced_calc/js/main.js
+++ advanced_calc/js/main.js
@@ -43,40 +43,30 @@ button1.addEventListener("click", function(){
 
 });
 
-// Advanced Calc
-var button1 = document.getElementById("basic-calc");
+// BMI CALC
+var button3 = document.getElementById("bmi-calc");
 button1.addEventListener("click", function(){
   console.log("click button event triggered");
 
-  var num1 = parseFloat(document.getElementById("basic-num-1").value);
-  console.log(num1)
-  var num2 = parseFloat(document.getElementById("basic-num-2").value);
-  console.log(num2)
-  var op = document.getElementById("basic-operation").value;
-  console.log(op)
+  var mass = parseFloat(document.getElementById("bmi-mass").value);
+  console.log(mass)
+  var height = parseFloat(document.getElementById("bmi-units").value);
+  console.log(height)
+  var units = document.getElementById("bmi-units").value;
+  console.log(units)
 
       var answer;
       // Calculate the answer based on the chosen basic operator
-      switch(op){
-        case "+":
-          answer = num1 + num2;
-          console.log(answer);
-          break;
-        case "-":
-          answer = num1 - num2;
-          break;
-        case "/":
-          answer = num1 / num2;
-          break;
-        case "*":
-          answer = num1 * num2;
-          break;
-      };
+      
+      bmi = mass / Math.pow(height, 2);
+      bmi = Math.round(bmi * 100) / 100;
+
+      if units = KG then if {units * 708}
       // finally, display the result
-  var basicAnswer = document.getElementById("basic-answer");
+  var bmiAnswer = document.getElementById("bmi-answer");
 
-  basicAnswer.className = "show";
+  bmiAnswer.className = "show";
 
-  document.getElementById("basic-answer-alert").innerHTML = "The answer is : " + answer;
+  document.getElementById("bmi-answer").innerHTML = "Your BMI is : " + bmi;
 
 });
